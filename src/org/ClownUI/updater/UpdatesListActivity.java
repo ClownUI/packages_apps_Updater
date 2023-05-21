LineageOS/android_packages_apps_Updater

Updater: Unbreak "Export update" feature
Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/4736
Change-Id: I186a27d2e99098cf8ebb3dd5c7348cdb528baa78

src/org/lineageos/updater/UpdatesListActivity.java
@@ -17,6 +17,9 @@ package org.lineageos.updater;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.lineageos.updater.model.UpdateInfo;
+
 public abstract class UpdatesListActivity extends AppCompatActivity {
+    public abstract void exportUpdate(UpdateInfo update);
     public abstract void showSnackbar(int stringId, int duration);
 }

ClownUI/packages_apps_Updater

Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

src/org/ClownUI/updater/UpdatesListActivity.java
@@ -17,6 +17,9 @@ package org.ClownUI.updater;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.protonplus.updater.model.UpdateInfo;
+
 public abstract class UpdatesListActivity extends AppCompatActivity {
+    public abstract void exportUpdate(UpdateInfo update);
     public abstract void showSnackbar(int stringId, int duration);
 }